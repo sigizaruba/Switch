e67b0a4ed0c01082ce5d70ff5dd138fcea978670
sigizaruba <56256755+sigizaruba@users.noreply.github.com>
2020-10-22 13:48:44 +0200

    Add files via upload

---
+++ TLE4964_3M/Blink_64_3M/AddOn_OpenCloseHallSwitch2Go.ino
@@ -0,0 +1,25 @@
+
+int state=LOW;
+  
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(14,OUTPUT);
+pinMode(9,INPUT);
+Serial.println("Start");
+digitalWrite(14,HIGH);
+delay(1000);
+digitalWrite(14,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+state=digitalRead(9);
+Serial.print("State: ");
+Serial.println(state); 
+
+digitalWrite(14,state);
+delay(100);
+}
